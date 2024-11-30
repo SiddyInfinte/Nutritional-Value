dd9b6c7e5c9d921d9596c25e84c4ea9513e7efd0
SiddyInfinte <gis.5097@gaursinternationalschool.com>
2024-11-30 12:54:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+activationButton = document.getElementById("dmode")
+navbar = document.getElementById("navbar")
+contentBox = document.getElementById("contentBox")
+
+function turnDarkMode() {
+    document.getElementById("navbar").style.backgroundColor = "white";
+    document.getElementById("navbar").style.color = "black"
+}
